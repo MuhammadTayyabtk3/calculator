af8e6bb05e46f5994947da838eb430c537100112
MuhammadTayyabtk3 <mtayyabtk3@gmail.com>
2025-08-02 08:27:02 -0700

    initial

---
+++ script.js
@@ -0,0 +1,102 @@
+// alert("hi")
+// window.alert("go")
+
+// alert("M")
+// alert("tayyan")
+// alert("Mtayyab@6785.gmail.com")
+// alert("03214698741")
+// alert("1234567")
+
+// alert("you're learing javascript@")
+
+// alert("dobara sa")
+
+// var firstName;
+
+// var idName;
+// idName="baba tillu"
+// alert(idName)
+
+// var teamName="1 ka 2"
+// alert(teamName);
+
+// var bestMan = "charlie"
+// bestMan="Be A man"
+
+// var caseQTy;
+
+// caseQTy=144;
+
+// var num = "9";
+
+// var onenum;
+
+// onenum=2 + 8;
+
+// alert(onenum)
+
+// var marttotal= 23;
+// var martcharges= 7;
+// var bill= martcharges + marttotal;
+// alert(bill)
+
+// var x= 9;
+// x= x - 5;
+// alert(x);
+
+// var num= 20 % 6;
+// alert(num)
+
+// var largeNum=1000 * 2000;
+// var result = largeNum;
+// alert(result)
+
+// var x =23 / 3;
+// var y = x;
+// alert(y);
+
+// alert( "result"+6*4)
+
+// var x=1;
+// x++
+// alert(x)
+
+// var y=100;
+// y--
+// alert(y)
+
+// var x = 50;
+// var y = ++x;
+// alert(y);
+
+// var y = 50;
+// var z = --y;
+// alert(z);
+
+// var A = 1;
+// var B = A;
+// alert(B)
+
+// var C = 5;
+// C++;
+// alert(C)
+
+// function show () {  
+//     document.getElementById('box').style.display='block'
+// }
+
+// function hide () {
+//     document.getElementById('box').style.display='none' 
+// }
+
+// function mystyle () {
+//     document.getElementById('para').style.fontSize='50px'
+//     document.getElementById('para').style.fontWeight='bolder'
+//     document.getElementById('para').style.fontFamily='sans-serif'
+// }
+
+var display = document.getElementById('display').innerHTML.valueOf
+console.log(display)
+
+
+
